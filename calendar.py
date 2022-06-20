770dec60cbbff890ca63c7412e9cd7cf8e01edc7
HarivinayAllada <107498061+HarivinayAllada@users.noreply.github.com>
2022-06-20 14:34:48 +0530

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,4 +1,5 @@
 #python program to display calendar of the given year
+#and checking whether it's a leap year or not
 
 #importing calendar module for calendar operations
 import calendar
@@ -9,3 +10,9 @@ yyyy = calendar.calendar(year,1,1,6,4)
 
 #prints or displays calender of the entered input from user
 print(yyyy)
+
+#prints whether it's a leap year or not
+if calendar.isleap(year):
+    print(year,"is a leap year")
+else:
+    print(year,"is not a leap year")
